112ebc3161f654b17db1e0bfa8738d1202296b92
TrungPt8 <160839826+TrungPt8@users.noreply.github.com>
2024-05-08 16:38:46 +0700

    Create Questions.java

---
+++ src/main/java/com/example/demo/Game/Questions.java
@@ -0,0 +1,20 @@
+package com.example.demo.Game;
+
+public class Questions {
+    private String question;
+    private String title;
+
+    public Questions(String question, String title) {
+        this.question = question;
+        this.title = title;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
